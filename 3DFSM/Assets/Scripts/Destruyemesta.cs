2d5c2bf95c22ddf2ff8188d11838e3d6729a62aa
Anghello Buendia <66330426+angbury@users.noreply.github.com>
2021-04-13 21:57:16 -0500

    130421

---
+++ 3DFSM/Assets/Scripts/Destruyemesta.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destruyemesta : MonoBehaviour
+{
+    //Referencias
+    public GameObject rota;
+    private void OnMouseDown()
+    {
+        //Spawnear la caja rota
+        Instantiate(rota, transform.position, transform.rotation);
+        //Desruirme (caja entera)
+        Destroy(gameObject);
+    }
+}
